4641e6e36abb93a178a701ebed12fba24a81afbc
vaishali <51531318+rana-vaishali@users.noreply.github.com>
2022-07-23 23:12:54 +0530

    products page

--- src/components/product.js
+++ src/components/product.js
@@ -15,25 +15,92 @@ import FormatItalicIcon from '@material-ui/icons/FormatItalic';
 import FormatUnderlinedIcon from '@material-ui/icons/FormatUnderlined';
 import FormatColorFillIcon from '@material-ui/icons/FormatColorFill';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 
 
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
   media: {
     height: 140,
   },
-});
+  category:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft:600,
+
+  },
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
 
 
 
 export default function Product() {
   const classes = useStyles();
+  const [age, setAge] = React.useState('');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
 
   return (
       <div>
+    <div className={classes.category}>
+    <ToggleButtonGroup >
+      <ToggleButton >ALL</ToggleButton>
+      <ToggleButton >APPAREL</ToggleButton>
+      <ToggleButton >ELECTRONICS</ToggleButton>
+      <ToggleButton >PERSONAL CARE</ToggleButton>
+    </ToggleButtonGroup>
+    </div>
+    <div>
+    <Button className={classes.button} onClick={handleOpen}>
+        <b>Sort By:</b>
+      </Button>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-controlled-open-select-label">Select...</InputLabel>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={age}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>Default</em>
+          </MenuItem>
+          <MenuItem value={10}>Price:High to Low</MenuItem>
+          <MenuItem value={20}>Price:Low to High</MenuItem>
+          <MenuItem value={30}>Newest</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+    <div>
     <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
@@ -57,7 +124,7 @@ export default function Product() {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button variant="contained" color="primary">
+        <Button path='/src/components/details.js' variant="contained" color="primary">
           BUY
         </Button>
         <IconButton
@@ -78,50 +145,7 @@ export default function Product() {
           </IconButton>
       </CardActions>
     </Card>
-     <Card className={classes.root}>
-     <CardActionArea>
-       <CardMedia
-         className={classes.media}
-         image="/static/images/cards/contemplative-reptile.jpg"
-         title="Contemplative Reptile"
-       />
-       <CardContent>
-           <CardActions>
-         <Typography gutterBottom variant="h6" component="h6" justify="flex-start">
-           Shoes
-         </Typography>
-         <Typography gutterBottom variant="h6" component="h6" direction="grid" justify="flex-end">
-           Shoes
-         </Typography>
-         </CardActions>
-         <Typography variant="body2" color="textSecondary" component="p">
-           Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-           across all continents except Antarctica
-         </Typography>
-       </CardContent>
-     </CardActionArea>
-     <CardActions>
-       <Button variant="contained" color="primary">
-         BUY
-       </Button>
-       <IconButton
-           edge="start"
-           className={classes.menuButton}
-           color="inherit"
-           aria-label="open drawer"
-         >
-           <EditIcon />
-         </IconButton>
-         <IconButton
-           edge="start"
-           className={classes.menuButton}
-           color="inherit"
-           aria-label="open drawer"
-         >
-           <DeleteIcon />
-         </IconButton>
-     </CardActions>
-   </Card>
+      </div>
    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 //import reportWebVitals from './reportWebVitals';
+import Product from './components/product';
+import Order from './components/createOrder';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+
+ReactDOM.render(<Order/>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
